83e7ed55b7a9db1a1c2c833b8b686b7fad3a9b3b
Mickael Daniel <daniel.mickael@gmail.com>
2011-10-09 15:33:04 +0200

    add git templates and `gi` alias
    templates only include a basic exclude config (node_modules,
    npm-debug.log) and a simple pre-commit hook that try to run `npm test`
    before letting the commit happen.

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -3,11 +3,12 @@ alias tm='top -o vsize' # memory
 
 ## git stuff
 alias g="git status -s"
-
 alias gg="source ~/.bashrc"
+alias gi="git init --template=$HOME/.dotfiles/git/templates"
 
 alias ls='ls -hF'
 alias ll='ls -lhF'
 alias la='ls -A'
 alias l='ls -lAhF'
 
+alias get='wget --no-check-certificate'
