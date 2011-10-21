43d6f7cb70390b70d624d00237ceb1322ecaf41b
Mickael Daniel <daniel.mickael@gmail.com>
2011-10-21 14:00:10 +0200

    tweak zsh configuration and add missing stuff from bash one

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,14 +1,23 @@
 
+function git-init {
+  rm -rf ${PWD}/.git/hooks/
+  git init --template=$HOME/.dotfiles/git/templates
+}
+
+
 alias tu='top -o cpu' # cpu
 alias tm='top -o vsize' # memory
 
 ## git stuff
 alias g="git status -s"
+alias gg="source ~/.zshrc"
 
-alias gg="source ~/.bashrc"
+# update git hooks, habe to remove file before running git init again to catch up changes
+alias gi="git-init"
 
 alias ls='ls -hF'
 alias ll='ls -lhF'
 alias la='ls -A'
 alias l='ls -lAhF'
 
+alias get='wget --no-check-certificate'
