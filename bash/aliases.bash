82bde121dea0378d3ba53fca1437ccec24309ce9
Mickael Daniel <daniel.mickael@gmail.com>
2012-05-19 13:20:53 +0200

    vim: mt to --remote-silent-tab, m to --remote-silent

--- bash/aliases.bash
+++ bash/aliases.bash
@@ -18,9 +18,14 @@ alias get='wget --no-check-certificate'
 
 alias t='todo -d ~/.dotfiles/todo/todo.cfg'
 
-# One vim... Just one. This ensures any files get opened in a "remote" vim, one
+# One vim... Just one.
+#
+# This ensures any files get opened in a "remote" vim, one
 # that were previously spawned (or create a new one if necessary). Opens the
-# files in a new tab
+# files in a new tab.
+#
+# mt to open with new tabs
+# m otherwise to open them all as buffers (:ls)
 #
 # see :h remote
 
@@ -31,4 +36,5 @@ if [[ $(which gvim) == "" ]]; then
   gvim="mvim"
 fi
 
-alias m="$gvim --remote-silent-tab ."
+alias m="$gvim --remote-silent"
+alias mt="$gvim --remote-silent-tab"
