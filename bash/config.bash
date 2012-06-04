85b6ed56f5bf6376aa25bb5ecab7023d01b5dbed
Mickael Daniel <daniel.mickael@gmail.com>
2012-06-04 18:12:56 +0200

    nave use 0.6.18 on source

--- bash/config.bash
+++ bash/config.bash
@@ -4,10 +4,14 @@ GIT_PS1_SHOWUNTRACKEDFILES=1
 GIT_PS1_SHOWSTASHSTATE=1
 GIT_PS1_SHOWUPSTREAM="verbose"
 
-nv=$(node --version)
-npmv=$(npm --version)
-
+# Add node / npm version in bash prompt
+#
+# nv=$(node --version)
+# npmv=$(npm --version)
+#
 #PS1='\w \[\033[0;90m\]« $nv/$npmv » $(__git_ps1 " \[\033[0;36m\](%s)")\[\033[0m\] » \n\[\033[0;90m\]\$ \[\033[0;00m\]'
+
+# without
 PS1='\[\033[0;90m\]\$\[\033[0;00m\] \w $(__git_ps1 "\[\033[0;90m\](%s)")\[\033[0m\] '
 
 
