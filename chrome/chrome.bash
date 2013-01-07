5debea3692e6106106f8a2d966f36dc0b3eca3ed
Mickael Daniel <daniel.mickael@gmail.com>
2013-01-07 11:32:09 +0100

    chrome alias

--- chrome/chrome.bash
+++ chrome/chrome.bash
@@ -1,11 +1,18 @@
 
 CHROME_DIR=$HOME/.config/google-chrome
 
+alias crdev='chrome-dev'
+
 function chrome() {
   google-chrome $@
 }
 
 function chrome-remote-debug() {
   echo google-chrome --user-data-dir=$CHROME_DIR-dev --remote-debugging-port=9222 --remote-debugging-frontend="http://$1/front-end/inspector.html"
-  google-chrome --user-data-dir=$CHROME_DIR-dev --remote-debugging-port=9222 --remote-debugging-frontend="$1"
+  google-chrome --user-data-dir=$CHROME_DIR-dev --remote-debugging-port=9222 --remote-debugging-frontend="http://$1/front-end/inspector.html"
+}
+
+
+function chrome-dev() {
+  chrome-remote-debug localhost:3000
 }
