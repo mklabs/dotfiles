ac98d47bb532d78b31bcac9315743b7fea8c6760
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-04 11:13:55 +0200

    Tweak prompt and git config

--- bash/env.bash
+++ bash/env.bash
@@ -2,15 +2,6 @@
 # Make .dotfiles/bin files executable
 export PATH="$HOME/.dotfiles/bin:$PATH"
 
-# add /opt/local/bin in front of /bin (to load updated verision of bash
-export PATH="/opt/local/bin:$PATH"
-
-# same for /usr/local/bin
-export PATH="/usr/local/bin:$PATH"
-
-# Colored `grep` output
-export GREP_OPTIONS='--color=always'
-
 # for `ls` (BSD, OSX)
 export CLICOLOR=1
 export LSCOLORS=gxfxcxdxbxegedabagacad
