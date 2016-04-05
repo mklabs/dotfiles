fe533bda86071e6d3166a3a03da480360fda57c7
Mickael Daniel <daniel.mickael@gmail.com>
2016-04-05 23:24:29 +0200

    Remove chrome, yum folders

--- welcome/welcome.bash
+++
@@ -1,2 +0,0 @@
-# Display the last commit id and message for the dotfile repo
-# git log --format=oneline -n 1 HEAD ~/.dotfiles
--- yum/yum.bash
+++
@@ -1,7 +0,0 @@
-# Should load only on yum like environment (rhel; centos, fedora, ...)
-
-[ -e /usr/bin/yum ] || return;
-
-alias ys="yum search"
-alias yi="sudo yum install"
-alias yay="sudo yum install -y"
